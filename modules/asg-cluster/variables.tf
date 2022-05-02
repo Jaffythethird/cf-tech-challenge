3d9d54445c0581eb2a1a17169d224825d0d47cdc
Jordan Fry <jordan.fry@infusionpoints.com>
2022-05-02 01:12:09 -0400

    Added variables passed into module

--- modules/asg-cluster/variables.tf
+++ modules/asg-cluster/variables.tf
@@ -1,2 +1,42 @@
-variable "vpc" {
+# Instance Info
+variable "ami" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "ebs-size" {
+    type = number
+}
+
+variable "user_data" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "private_key" {
+    type = string
+}
+
+# Networking
+variable "vpc_security_group_ids" {
+    type = list(string)
+}
+
+variable "subnet_ids" {
+    type = list(string)
+}
+
+# ASG/ALB Values
+variable "asg_min" {
+    type = number
+}
+
+variable "asg_max" {
+    type = number
 }
