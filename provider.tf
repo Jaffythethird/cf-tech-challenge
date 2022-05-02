de848e3435a49733aa9c9af39e11f705c90c6b0f
Jordan Fry <jordan.fry@infusionpoints.com>
2022-05-02 01:13:55 -0400

    Added variables for auth

--- provider.tf
+++ provider.tf
@@ -9,4 +9,7 @@ terraform {
 
 provider "aws" {
   region = var.region
+  profile = var.profile
+  shared_config_files = var.shared_config_files
+  shared_credentials_files = var.shared_credentials_files
 }
