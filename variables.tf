76aa2350eb0a93c11069d5c964ab32c879de6dec
Jordan Fry <jordan.fry@infusionpoints.com>
2022-05-02 01:15:48 -0400

    removed quotes from types

--- variables.tf
+++ variables.tf
@@ -1,38 +1,38 @@
 variable "region" {
     description = "The AWS region to build the environment in"
-    type = "string"
+    type = string
     default = "us-east-1"
 }
 
 variable "rhel_ami" {
     description = "The AMI to be used for Red Hat Linux instances"
-    type = "string"
+    type = string
     default = "ami-0b0af3577fe5e3532"
 }
 
 variable "pub_key_path" {
     description = "The path from your running directory to the public ssh key file to be passed to the instances"
-    type = "string"
+    type = string
 }
 
 variable "private_key_path" {
     description = "The path from your running directory to the private ssh key file to be passed to the instances"
-    type = "string"
+    type = string
 }
 
 variable "profile" {
     description = "The name of the profile as stated in your AWS Credentials/Config file"
-    type = "string"
+    type = string
 }
 
 variable "shared_config_files" {
     description = "The path to your .aws config file"
-    type = "string"
+    type = string
     default = "~/.aws/config"
 }
 
 variable "shared_credentials_files" {
     description = "The path to your .aws credentials file"
-    type = "string"
+    type = string
     default = "~/.aws/credentials"
 }
